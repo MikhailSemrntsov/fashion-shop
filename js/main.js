23f0be14d47909b9affa2cf2411cf5af28dc31c6
semrncovmikhail <delaporabote@mail.ru>
2021-01-09 20:44:00 +0300

    create: section customs with adaptive

--- js/main.js
+++ js/main.js
@@ -1,17 +1,45 @@
-$(document).ready(function(){
-var menuButton = document.querySelector(".button__menu");
-menuButton.addEventListener("click", function (){
-  console.log("клик по кнопке меню");
-  document
-  .querySelector(".navbar")
-  .classList.toggle("navbar__visible");
-});
+$(document).ready(function () {
+  var menuButton = document.querySelector(".button__menu");
+  menuButton.addEventListener("click", function () {
+    console.log("клик по кнопке меню");
+    document.querySelector(".navbar").classList.toggle("navbar__visible");
+  });
 
-var closeMenuButton = $(".navbar__icon-close");
-closeMenuButton.on("click", closeMenu);
+  var closeMenuButton = $(".navbar__icon-close");
+  closeMenuButton.on("click", closeMenu);
 
-function closeMenu(){
-  var menuClose = $(".navbar");
-  menuClose.removeClass("navbar__visible");
-}
+  function closeMenu() {
+    var menuClose = $(".navbar");
+    menuClose.removeClass("navbar__visible");
+  }
+
+  // tabs
+
+  var tabsCategory = $(".category__text");
+  var tabsCards = $(".cards__product");
+
+  tabsCategory.on("click", function (event) {
+    event.preventDefault();
+    var activeContent = $(this).attr("data-target");
+    tabsCards.removeClass("cards__product--active");
+    tabsCategory.removeClass("category__text--active");
+    $(activeContent).addClass("cards__product--active");
+    $(this).addClass("category__text--active");
+  });
+
+  // swiper-slider
+
+  var mySwiper = new Swiper(".swiper-container", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    // autoplay: {
+    //   delay: 2000,
+    // },
+  });
 });
